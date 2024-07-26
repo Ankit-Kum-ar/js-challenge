0b614ac17b180fd92702fd16eb6a29d4f71ce61b
Ankit-0009 <ankitcode2511@gmail.com>
2024-07-26 12:30:30 +0530

    first commit

---
+++ day_1.js
@@ -0,0 +1,43 @@
+// Task 1
+var num = 45;
+console.log("Number is",num);
+
+// Task 2
+let person = "John";
+console.log("Person is",person);
+
+// Task 3
+const value = false;
+console.log("Value is",value);
+
+// Task 4
+const count = 5;
+const message = "This is JS challenge";
+const flag = true;
+const student = {
+    name: "Ankit",
+    age: 21,
+    city: "Delhi",
+    country: "India",
+    isStudent: true,
+}
+const list = [4,3,2,1,3,5];
+
+console.log(typeof(count));
+console.log(typeof(message));
+console.log(typeof(flag));
+console.log(typeof(student));
+console.log(typeof(list));
+console.log(Array.isArray(list));
+
+// Task 5
+let temp = 56;
+console.log(temp);
+temp = "This is a string";
+console.log(temp);
+
+// Task 6
+const test = 544;
+console.log(test);
+test = 123; // Can't assign due to const property of js.
+console.log(test);
